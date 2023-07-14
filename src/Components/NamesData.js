06c246f18aa50bf3eeb4eb4eb7712351ea6b149b
Marryggk <99277162+Meronggk@users.noreply.github.com>
2023-07-14 11:18:04 +0100

    added names data

--- src/Components/NamesData.js
+++ src/Components/NamesData.js
@@ -134,5 +134,255 @@ const NamesData = [
         name: "Arsu",
         sex: "f",
       },
+      {
+        id: 27,
+        name: "Zahra",
+        sex: "f",
+      },
+      {
+        id: 28,
+        name: "Jhon",
+        sex: "m",
+      },
+      {
+        id: 29,
+        name: "Anosh",
+        sex: "f",
+      },
+      {
+        id: 30,
+        name: "Selam",
+        sex: "f",
+      },
+      {
+        id: 31,
+        name: "Adam",
+        sex: "m",
+      },
+      {
+        id: 32,
+        name: "Yasir",
+        sex: "m",
+      },
+      {
+        id: 33,
+        name: "Danu",
+        sex: "f",
+      },
+      {
+        id: 34,
+        name: "Abbie",
+        sex: "m",
+      },
+      {
+        id: 35,
+        name: "Sahar",
+        sex: "f",
+      },
+      {
+        id: 36,
+        name: "Neo",
+        sex: "m",
+      },
+      {
+        id: 37,
+        name: "Meron",
+        sex: "f",
+      },
+      {
+        id: 38,
+        name: "Olu",
+        sex: "m",
+      },
+      {
+        id: 39,
+        name: "Ellen",
+        sex: "f",
+      },
+      {
+        id: 40,
+        name: "Daz",
+        sex: "f",
+      },
+      {
+        id: 41,
+        name: "Nabil",
+        sex: "m",
+      },
+      {
+        id: 42,
+        name: "Kheri",
+        sex: "f",
+      },
+      {
+        id: 43,
+        name: "Kidus",
+        sex: "m",
+      },
+      {
+        id: 44,
+        name: "Aiyana",
+        sex: "f",
+      },
+      {
+        id: 45,
+        name: "Ayana",
+        sex: "f",
+      },
+      {
+        id: 46,
+        name: "Makoa",
+        sex: "m",
+      },
+      {
+        id: 47,
+        name: "Cyrus",
+        sex: "m",
+      },
+      {
+          id: 48,
+          name: "Brad",
+          sex: "m",
+        },
+        {
+          id: 49,
+          name: "Nick",
+          sex: "m",
+        },
+        {
+          id: 50,
+          name: "Dmet",
+          sex: "f",
+        },
+        {
+          id: 51,
+          name: "Yordu",
+          sex: "f",
+        },
+        {
+            id: 52,
+            name: "Zahra",
+            sex: "f",
+          },
+          {
+            id: 53,
+            name: "Jhon",
+            sex: "m",
+          },
+          {
+            id: 54,
+            name: "Anosh",
+            sex: "f",
+          },
+          {
+            id: 55,
+            name: "Selam",
+            sex: "f",
+          },
+          {
+            id: 56,
+            name: "Adam",
+            sex: "m",
+          },
+          {
+            id: 57,
+            name: "Yasir",
+            sex: "m",
+          },
+          {
+            id: 58,
+            name: "Danu",
+            sex: "f",
+          },
+          {
+            id: 59,
+            name: "Abbie",
+            sex: "m",
+          },
+          {
+            id: 60,
+            name: "Sahar",
+            sex: "f",
+          },
+          {
+            id: 61,
+            name: "Neo",
+            sex: "m",
+          },
+          {
+            id: 62,
+            name: "Meron",
+            sex: "f",
+          },
+          {
+            id: 63,
+            name: "Olu",
+            sex: "m",
+          },
+          {
+            id: 64,
+            name: "Ellen",
+            sex: "f",
+          },
+          {
+            id: 65,
+            name: "Daz",
+            sex: "f",
+          },
+          {
+            id: 66,
+            name: "Nabil",
+            sex: "m",
+          },
+          {
+            id: 67,
+            name: "Kheri",
+            sex: "f",
+          },
+          {
+            id: 68,
+            name: "Kidus",
+            sex: "m",
+          },
+          {
+            id: 69,
+            name: "Aiyana",
+            sex: "f",
+          },
+          {
+            id: 70,
+            name: "Ayana",
+            sex: "f",
+          },
+          {
+            id: 71,
+            name: "Makoa",
+            sex: "m",
+          },
+          {
+            id: 72,
+            name: "Cyrus",
+            sex: "m",
+          },
+          {
+              id: 73,
+              name: "Brad",
+              sex: "m",
+            },
+            {
+              id: 74,
+              name: "Nick",
+              sex: "m",
+            },
+            {
+              id: 75,
+              name: "Dmet",
+              sex: "f",
+            },
+            {
+              id: 76,
+              name: "Yordu",
+              sex: "f",
+            },
 ];
 export default NamesData;  
